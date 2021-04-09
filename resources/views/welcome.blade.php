4b0d973b53f0d8aa8b8c5d3875dc9409d6b2e0ab
Painkiller-18 <al221810675@gmail.com>
2021-04-09 01:54:01 -0500

    First commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,167 @@
+<!DOCTYPE html>
+<html class="no-js" lang="zxx">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>MATEMÁTICAS PARA INGENIERÍA </title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="manifest" href="site.webmanifest">
+    <link rel="shortcut icon" type="image/x-icon" href="assets/img/favicon.ico')}}">
+
+    <!-- CSS here -->
+    <link rel="stylesheet" href="{{asset('original/assets/css/bootstrap.min.css')}}">
+    <link rel="stylesheet" href="{{asset('original/assets/css/owl.carousel.min.css')}}">
+    <link rel="stylesheet" href="{{asset('original/assets/css/slicknav.css')}}">
+    <link rel="stylesheet" href="{{asset('original/assets/css/flaticon.css')}}">
+    <link rel="stylesheet" href="{{asset('original/assets/css/progressbar_barfiller.css')}}">
+    <link rel="stylesheet" href="{{asset('original/assets/css/gijgo.css')}}">
+    <link rel="stylesheet" href="{{asset('original/assets/css/animate.min.css')}}">
+    <link rel="stylesheet" href="{{asset('original/assets/css/animated-headline.css')}}">
+    <link rel="stylesheet" href="{{asset('original/assets/css/magnific-popup.css')}}">
+    <link rel="stylesheet" href="{{asset('original/assets/css/fontawesome-all.min.css')}}">
+    <link rel="stylesheet" href="{{asset('original/assets/css/themify-icons.css')}}">
+    <link rel="stylesheet" href="{{asset('original/assets/css/slick.css')}}">
+    <link rel="stylesheet" href="{{asset('original/assets/css/nice-select.css')}}">
+    <link rel="stylesheet" href="{{asset('original/assets/css/style.css')}}">
+    
+</head>
+<body>
+    <!-- ? Preloader Start -->
+    <div id="preloader-active">
+        <div class="preloader d-flex align-items-center justify-content-center">
+            <div class="preloader-inner position-relative">
+                <div class="preloader-circle"></div>
+                <div class="preloader-img pere-text">
+                    <img src="{{asset('original/assets/img/logo/loder.png')}}" alt="">
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Preloader Start -->
+    <header>
+        <!-- Header Start -->
+        <!-- Header End -->
+    </header>
+    <main>
+        <!-- Slider Area Start-->
+        <div class="slider-area slider-bg ">
+            <div class="slider-active">
+                <!-- Single Slider -->
+                <div class="single-slider d-flex align-items-center slider-height ">
+                    <div class="container">
+                        <div class="row align-items-center justify-content-between">
+                            <div class="col-xl-6 col-lg-6 col-md-9 ">
+                                <div class="hero__caption">
+                                    <span data-animation="fadeInLeft" data-delay=".3s">Roxana Perez Torres</span>
+                                    <h1 data-animation="fadeInLeft" data-delay=".6s">MATEMÁTICAS PARA INGENIERÍA</h1>
+                                    <p data-animation="fadeInLeft" data-delay=".8s">UNIDAD III</p>
+                                    <p data-animation="fadeInLeft" data-delay=".8s">Martinez Becerril Javier.</p>
+                                    <p data-animation="fadeInLeft" data-delay=".8s">Ing. Desarrollo y Gestion de Software.</p>
+                                    <!-- Slider btn -->
+                                </div>
+                            </div>
+                            <div class="col-xl-6 col-lg-6">
+                                <div class="hero__img d-none d-lg-block f-right">
+                                    <img src="{{asset('original/assets/img/hero/hero_right.png')}}" alt="" data-animation="fadeInRight" data-delay="1s">
+                                </div>
+                            </div>
+                            <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-xl-12">
+                    <div class="section-tittle center mb-105">
+                        <h2>Valores de entrada</h2>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-4 col-md-4 col-sm-6"">
+                <form action="#">
+                    <div class="single-cat">
+                        
+                        <div class="mt-10">
+						<input type="text" name="iteraciones" placeholder="Iteraciones"
+						onfocus="this.placeholder = ''" onblur="this.placeholder = 'Iteraciones'" required
+						class="single-input">
+					    </div>                    
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-4 col-sm-6">
+                    <div class="single-cat">
+                        <div class="mt-10">
+							<input type="text" name="h" placeholder="Valor de H"
+							onfocus="this.placeholder = ''" onblur="this.placeholder = 'Valor de H'" required
+							class="single-input">
+						</div>
+                    </div>
+                </div>
+
+                <div class="col-lg-4 col-md-4 col-sm-6">
+                <div class="single-cat">
+                        <div class="mt-19">
+                        <li class="button-header margin-left "><a href="register.html" class="btn">Aceptar</a></li>
+						</div>
+                    </div>
+                </div>
+                </form>
+            </div>
+        </div>       
+
+    <!--? Team -->
+                    <section class="team-area section-padding40 section-bg1">                   
+                    </section>
+
+  <!-- Scroll Up -->
+  <div id="back-top" >
+    <a title="Go to Top" href="#"> <i class="fas fa-level-up-alt"></i></a>
+</div>
+
+<!-- JS here -->
+
+<script src="{{asset('original/assets/js/vendor/modernizr-3.5.0.min.js')}}"></script>
+<!-- Jquery, Popper, Bootstrap -->
+<script src="{{asset('original/assets/js/vendor/jquery-1.12.4.min.js')}}"></script>
+<script src="{{asset('original/assets/js/popper.min.js')}}"></script>
+<script src="{{asset('original/assets/js/bootstrap.min.js')}}"></script>
+<!-- Jquery Mobile Menu -->
+<script src="{{asset('original/assets/js/jquery.slicknav.min.js')}}"></script>
+
+<!-- Jquery Slick , Owl-Carousel Plugins -->
+<script src="{{asset('original/assets/js/owl.carousel.min.js')}}"></script>
+<script src="{{asset('original/assets/js/slick.min.js')}}"></script>
+<!-- One Page, Animated-HeadLin -->
+<script src="{{asset('original/assets/js/wow.min.js')}}"></script>
+<script src="{{asset('original/assets/js/animated.headline.js')}}"></script>
+<script src="{{asset('original/assets/js/jquery.magnific-popup.js')}}"></script>
+
+<!-- Date Picker -->
+<script src="{{asset('original/assets/js/gijgo.min.js')}}"></script>
+
+<!-- Video bg -->
+<script src="{{asset('original/assets/js/jquery.vide.js')}}"></script>
+
+<!-- Nice-select, sticky -->
+<script src="{{asset('original/assets/js/jquery.nice-select.min.js')}}"></script>
+<script src="{{asset('original/assets/js/jquery.sticky.js')}}"></script>
+<!-- Progress -->
+<script src="{{asset('original/assets/js/jquery.barfiller.js')}}"></script>
+
+<!-- counter , waypoint,Hover Direction -->
+<script src=".{{asset('original/assets/js/jquery.counterup.min.js')}}"></script>
+<script src="{{asset('original/assets/js/waypoints.min.js')}}"></script>
+<script src="{{asset('original/assets/js/jquery.countdown.min.js')}}"></script>
+<script src="{{asset('original/assets/js/hover-direction-snake.min.js')}}"></script>
+
+<!-- contact js -->
+<script src="{{asset('original/assets/js/contact.js')}}"></script>
+<script src="{{asset('original/assets/js/jquery.form.js')}}"></script>
+<script src="{{asset('original/assets/js/jquery.validate.min.js')}}"></script>
+<script src="{{asset('original/assets/js/mail-script.js')}}"></script>
+<script src="{{asset('original/assets/js/jquery.ajaxchimp.min.js')}}"></script>
+
+<!-- Jquery Plugins, main Jquery -->	
+<script src="{{asset('original/assets/js/plugins.js')}}"></script>
+<script src="{{asset('original/assets/js/main.js')}}"></script>
+
+</body>
+</html>
